456dcd0ca83a4cca66eee8cbdfd348c6779d7fda
Elijah Miller <jqr@gibson.(none)>
2008-05-03 20:48:18 -0400

    Initial import.

---
+++ postgres_sequences.rb
@@ -0,0 +1,41 @@
+module ActiveRecord
+  class Base
+    def self.id_sequence
+      "#{table_name}_id_seq"
+    end
+    
+    # Returns the next id on the table's id sequence.
+    def self.next_id
+      connection.next_in_sequence(id_sequence)
+    end
+    
+    def self.current_id
+      connection.current_in_sequence(id_sequence)
+    end
+
+    def self.last_id
+      connection.current_in_sequence(id_sequence)
+    end
+  end
+  
+  module ConnectionAdapters
+    class PostgreSQLAdapter 
+      # Returns the next value in sequence.
+      def next_in_sequence(sequence)
+        select_value("SELECT nextval('#{sequence}')").to_i
+      end
+
+      def current_in_sequence(sequence)
+        select_value("SELECT currval('#{sequence}')").to_i
+      end
+
+      def current_in_sequence(sequence)
+        select_value("SELECT lastval('#{sequence}')").to_i
+      end
+    
+      def set_sequence(sequence, value, value_used = true)
+        select_value("SELECT setval('#{sequence}', #{value}, #{value_used})")
+      end
+    end
+  end
+end
