d5eef4b1b1aebf515376d35d0a7414ee3c444adc
Jonathan Tron <jonathan.tron@akiraweb.lu>
2009-01-11 20:27:40 +0800

    Add some dc
    Signed-off-by: Elijah Miller <elijah.miller@gmail.com>

--- postgres_sequences.rb
+++ postgres_sequences.rb
@@ -3,12 +3,12 @@ module ActiveRecord
     def self.id_sequence
       connection.default_sequence_name
     end
-    
+
     # Returns the next id on the table's id sequence.
     def self.next_id
       connection.next_in_sequence(id_sequence)
     end
-    
+
     def self.current_id
       connection.current_in_sequence(id_sequence)
     end
@@ -17,7 +17,7 @@ module ActiveRecord
       connection.current_in_sequence(id_sequence)
     end
   end
-  
+
   module ConnectionAdapters
     class PostgreSQLAdapter 
       # Returns the next value in sequence.
@@ -32,19 +32,23 @@ module ActiveRecord
       def current_in_sequence(sequence)
         select_value("SELECT lastval(#{quote_table_name(sequence)})").to_i
       end
-    
+
       def set_sequence(sequence, value, value_used = true)
         select_value("SELECT setval(#{quote_table_name(sequence)}, #{quote(value)}, #{quote(value_used)})")
       end
-      
+
       def create_sequence(sequence, start = nil)
         options = ''
-        options << " START #{quote(start)}" if start # Set start value (ie: value returned by next_in_sequence next time)
-        execute "CREATE SEQUENCE #{quote_table_name(sequence)} #{options}"
+        options << " START #{quote(start)}" if start # Set start value (ie: value returned by next_in_sequence on next call)
+        execute "CREATE SEQUENCE #{quote_table_name(sequence)} #{options}" unless sequence_exists?(sequence)
       end
-      
+
       def drop_sequence(sequence)
         execute "DROP SEQUENCE IF EXISTS #{quote_table_name(sequence)}"
+      end
+
+      def sequence_exists?(sequence)
+        query("SELECT relname FROM pg_class WHERE relkind='S'").flatten.include?(sequence)
       end
     end
   end
